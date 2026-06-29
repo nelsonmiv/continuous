a36ea4fd5e805fd517cab46892bae0bd9668aed0
nelsonmiv <nelsonmiv@gmail.com>
2026-06-29 02:31:20 -0400

    Delete server.ts

--- server.ts
+++
@@ -1,6 +0,0 @@
-{
-  "name": "Present Continuous Master",
-  "description": "Una aplicación educativa e interactiva para practicar el presente continuo con autoevaluación de pronunciación y sincronización en Google Sheets.",
-  "requestFramePermissions": ["microphone"],
-  "majorCapabilities": ["MAJOR_CAPABILITY_SERVER_SIDE_GEMINI_API"]
-}
