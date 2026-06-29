c7111d2ee4f4c55d27c9073f888d27423365b2b3
nelsonmiv <nelsonmiv@gmail.com>
2026-06-29 02:25:57 -0400

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,6 @@
+{
+  "name": "Present Continuous Master",
+  "description": "Una aplicación educativa e interactiva para practicar el presente continuo con autoevaluación de pronunciación y sincronización en Google Sheets.",
+  "requestFramePermissions": ["microphone"],
+  "majorCapabilities": ["MAJOR_CAPABILITY_SERVER_SIDE_GEMINI_API"]
+}
